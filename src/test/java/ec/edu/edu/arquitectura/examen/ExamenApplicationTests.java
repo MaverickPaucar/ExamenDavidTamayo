5418cb23a02394e82c66fb3c7244eae7d2018fe7
datamayo <65564598+datamayo@users.noreply.github.com>
2023-08-25 11:13:16 -0500

    with docker configurations

--- src/test/java/ec/edu/edu/arquitectura/examen/ExamenApplicationTests.java
+++ src/test/java/ec/edu/edu/arquitectura/examen/ExamenApplicationTests.java
@@ -3,7 +3,7 @@ package ec.edu.edu.arquitectura.examen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ExamenApplicationTests {
 
 	@Test
